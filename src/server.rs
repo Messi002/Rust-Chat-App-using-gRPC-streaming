d07e6514dec91323b16605010a4519e44271ab97
Messi002 <rostandmessi2@gmail.com>
2024-07-22 14:30:00 +0100

    hotfix an issue with chat_message_streaming fn

--- src/server.rs
+++ src/server.rs
@@ -4,6 +4,8 @@ pub mod pb{
 
 
 use std::net::ToSocketAddrs;
+use std::result;
+use futures_util::future::ok;
 use tokio::sync::mpsc;
 use tokio_stream::{Stream, StreamExt};
 use tokio_stream::wrappers::ReceiverStream;
@@ -23,7 +25,39 @@ type RepsonseStream = Pin<Box<dyn Stream<Item = Result<ChatMessage, Status>> + S
 
 type ChatResult<T> = Result<Response<T>, Status>;
 
+#[tonic::async_trait]
+impl pb::chat_service_server::ChatService for ChatServer{
 
+    type ChatMessageStreamingStream = RepsonseStream;
+
+    async fn chat_message_streaming(&self, request: Request<Streaming<ChatMessage>>)-> ChatResult<Self::ChatMessageStreamingStream>{
+        let mut in_stream = request.into_inner();
+
+        let (tx, rx) = mpsc::channel(128);
+
+        tokio::spawn(async move{
+            while let Some(result) = in_stream.next().await  {
+               match result {
+                Ok(item) => {
+                    println!("Received {:?} from {:?}", item.message, item.from);
+                    tx.send(
+                        Ok(ChatMessage{
+                            message: format!("{:?} Server", item.message),
+                            from : String::from("Server side")
+                        })
+                    ).await.unwrap();
+                }
+
+                Err() => {
+                    println!("Error: {}", status);
+                    break;
+                }
+               }
+
+            }
+        })
+    }
+}
 fn  main() {
     
 }
