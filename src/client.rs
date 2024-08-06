cb60dde49db68958c6319502692d5fc936926b5e
Messi002 <rostandmessi2@gmail.com>
2024-08-06 14:30:00 +0100

    updated server.rs

--- src/client.rs
+++ src/client.rs
@@ -1,18 +1,15 @@
-pub mod pb{
+pub mod pb {
     tonic::include_proto!("chat");
 }
 
-
-use futures_util::future::ok;
 use tokio::io;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio_stream::StreamExt;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::ReceiverStream;
-use tonic::{Streaming, Request, Response, Status};
+use tonic::Request;
 use tonic::transport::Channel;
 use pb::{chat_service_client::ChatServiceClient, ChatMessage};
-
 
 pub async fn input() -> String {
     println!("Type something... !");
@@ -20,20 +17,19 @@ pub async fn input() -> String {
     let mut inp = String::new();
     let stdin = io::stdin();
     let mut reader = BufReader::new(stdin);
-    reader.read_line(& mut inp).await.expect("Failed to read line");
+    reader.read_line(&mut inp).await.expect("Failed to read line");
     inp.trim().to_string()
 }
 
-
-async fn chat(client: & mut ChatServiceClient<Channel>){
-    let (tx, rx) = mpsc::channel(128); 
+async fn chat(client: &mut ChatServiceClient<Channel>) {
+    let (tx, rx) = mpsc::channel(128);
 
     let in_stream = ReceiverStream::new(rx);
 
-    tokio::spawn(async move{
-        loop{
+    tokio::spawn(async move {
+        loop {
             let user_msg = input().await;
-            if user_msg.eq_ignore_ascii_case("exit"){
+            if user_msg.eq_ignore_ascii_case("exit") {
                 break;
             } else {
                 let msg = ChatMessage {
@@ -41,11 +37,10 @@ async fn chat(client: & mut ChatServiceClient<Channel>){
                     from: String::from("Client"),
                 };
 
-                if tx.send(msg).await.is_err(){
+                if tx.send(msg).await.is_err() {
                     break;
                 }
             }
-            
         }
     });
 
@@ -53,22 +48,18 @@ async fn chat(client: & mut ChatServiceClient<Channel>){
 
     let mut resp_stream = response.into_inner();
 
-    while let Some(receive) = resp_stream.next().await{
-       let item = receive.unwrap();
-       println!("Received {:?} from {:?}", item.message, item.from);
+    while let Some(receive) = resp_stream.next().await {
+        let item = receive.unwrap();
+        println!("Received {:?} from {:?}", item.message, item.from);
     }
 }
 
-
 #[tokio::main]
-async fn main()-> Result<(), Box<dyn std::error::Error>>{
- let mut client = ChatServiceClient::connect( "http://[::1]:50051").await.unwrap();
- println!("Client Started");
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = ChatServiceClient::connect("http://[::1]:50051").await.unwrap();
+    println!("Client Started");
 
- chat(&mut client).await;
+    chat(&mut client).await;
 
-
- ok(())
-
-
+    Ok(())
 }
